1e4b40fa5c9e23d71245679114487be33ece1c46
Nick Rentschler <nickrench3@icloud.com>
2019-10-14 19:33:15 -0500

    Added solution

---
+++ SavingsUserManager/SavingsUserManager/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace SavingsUserManager
+{
+    public partial class Form1 : Form
+    {
+        private SqlConnection con = new SqlConnection(@"Data Source=NICKRENTSCHLER\SQLEXPRESS;Initial Catalog=Savings;Integrated Security=True;Pooling=False");
+        private SqlCommand cmd;
+
+        public Form1()
+        {
+            InitializeComponent();
+            fillCombo();
+        }
+
+        void fillCombo()
+        {
+            try
+            {
+                con.Open();
+                cmd = new SqlCommand("SELECT * FROM Login", con);
+                SqlDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    string userName = (dr["USERNAME"].ToString());
+                    comboBox1.Items.Add(userName);
+
+                }
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            string message = comboBox1.Text + " Deleted";
+            MessageBox.Show(message, "Delete User", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            string user = comboBox1.Text.Trim();
+            con.Open();
+            cmd = new SqlCommand("DELETE Login WHERE USERNAME='" + user + "'", con);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            comboBox1.Text = "";
+            comboBox1.Items.Clear();
+            fillCombo();
+        }
+    }
+}
